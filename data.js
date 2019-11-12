07d70d60c3ab14d045ea8f68b2392a6a495ccd4a
amrojjeh <amrojjeh@gmail.com>
2019-11-11 19:01:58 -0600

    Reform

---
+++ data.js
@@ -0,0 +1,35 @@
+var Projects = 
+{
+	"Software" : 
+	[
+		{
+			"Name" : "Job Tracker",
+			"Description" : "This is an application that is meant to track any person's job by keeping logs of when the shift starts and ends. The goal was to write it as modular as possible, while also including unit tests. However, due to a lack of time, this project has been cancelled. I'll consider picking it up once again later.",
+			"ImagePath" : "JobTracker.png",
+			"Link" : "https://github.com/dinglydo/JobTracker",
+			"Date" : "October 2019" // Latest update
+		},
+
+		{
+			"Name": "Chip8 Interpreter",
+			"Description" : "This program is meant to interpret basic chip8 instructions. Everything works except for sound. It's written in C++ and SDL. Chip8 is a virtual processor that is meant to teach programmers how to emulate CPU instructions.",
+			"ImagePath": "Chip8.jpg",
+			"Link" : "https://github.com/dinglydo/Chip8-Interpreter",
+			"Date" : "May 2018"
+		},
+	],
+
+	"Articles":
+	[
+		{
+			"Name" : "What Are Moles?",
+			"Description" : "Describes what a mole is and what it isn't, in a single page. Article was ported from my site to Medium for easier access.",
+			"ImagePath" : "Moles.jpg",
+			"Link" : "https://medium.com/@amrojjeh/what-are-moles-ec867a145ad5",
+			"Date" : "November 2018"
+		}
+	]
+}
+
+var SelectedCategory = "Articles";
+var projectsElement = document.getElementById("projects");
---
+++ index.js
@@ -0,0 +1,73 @@
+// Global variables are defined in data.js
+
+init();
+
+function init() // Initializes the site by loading everything neccessary
+{
+	renderCategories();
+	loadCategories();
+	select(Projects[SelectedCategory].Link);
+	loadProjects(SelectedCategory, projectsElement);
+}
+
+function renderCategories()
+{
+	for (var key in Projects)
+	{
+		var html = "<li id='" + key + "'onclick='categoryChangeEvent(this)' class>" + key + "</li>";
+		categories.innerHTML += html;
+	}
+}
+
+function loadCategories() // Loads the HTML document of the categories
+{
+	var categories = document.getElementById("categories");
+	for (var key in Projects)
+		Projects[key].Link = document.getElementById(key);
+}
+
+function select(element) // Underlines the category in CSS, hence why it's "selected"
+{
+	for (var key in Projects)
+		if (Projects[key].Link == element)
+		{
+			element.className = "selected";
+			SelectedCategory = key;
+		}
+		else Projects[key].Link.className = "";
+}
+
+function loadProjects(category, parent) // Loads projects from Projects and displays them according to the category
+{
+	parent.innerHTML = "";
+	for (var index in Projects[category])
+	{
+		if (index != "Link")
+		addProject(Projects[category][index], parent);
+	}
+}
+
+function addProject(project, parent) // Adds a project and displays
+{
+	var html = generateHTML(project.Name, project.Description, project.ImagePath, project.Link, project.Date);
+	parent.innerHTML += html;
+}
+
+function generateHTML(name, description, imagePath, link, date) // Generates the HTML of a project
+{
+	var header;
+	if (imagePath == "None")
+		header = '<div><a href="' + link + '"></a>';
+	else
+		header = '<div><a href="' + link + '"><img src="' + imagePath + '"></a>';
+	var h1 = '<h1><a href="' + link + '">' + name + '</a></h1>';
+	var date = "<i>Last updated: " + date + "</i>"
+	var p = "<p>" + description + "</p>";
+	return header + h1 + date + p + "</div>";
+}
+
+function categoryChangeEvent(element)
+{
+	select(element);
+	loadProjects(SelectedCategory, projectsElement);
+}
