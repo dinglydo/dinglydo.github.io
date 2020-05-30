163c6f675b2b683eb29004fedc6d0dc095ffb9a6
amrojjeh <amrojjeh@gmail.com>
2020-05-29 21:55:27 -0500

    Added a new project

--- data.js
+++ data.js
@@ -3,6 +3,13 @@ var Projects =
 	"Software" : 
 	[
 		{
+			"Name" : "Party bot",
+			"Description" : "A Town of Salem inspired discord bot. Work in progress",
+			"ImagePath" : "bot.jpg",
+			"Link" :"https://github.com/Persian-Eagle/Town-of-Salem-Bot",
+			"Date" : "May 2020"
+		},
+		{
 			"Name" : "Job Tracker",
 			"Description" : "This is an application that is meant to track any person's job by keeping logs of when the shift starts and ends. The goal was to write it as modular as possible, while also including unit tests. However, due to a lack of time, this project has been cancelled.",
 			"ImagePath" : "JobTracker.png",
@@ -16,17 +23,17 @@ var Projects =
 			"ImagePath": "Chip8.jpg",
 			"Link" : "https://github.com/dinglydo/Chip8-Interpreter",
 			"Date" : "May 2018"
-		},
+		}
 	],
 
 	"Articles":
 	[
 		{
-			"Name" : "What Are Moles?",
-			"Description" : "Describes what a mole is and what it isn't, in a single page. Article was ported from my site to Medium for easier access.",
-			"ImagePath" : "Moles.jpg",
-			"Link" : "https://medium.com/@amrojjeh/what-are-moles-ec867a145ad5",
-			"Date" : "November 2018"
+			"Name" : "1024 or 1000?",
+			"Description" : "There is a misunderstanding that occurs much too often between developers. Is a kilobit 1024 or 1000 bits?",
+			"ImagePath" : "1024 or 1000.png",
+			"Link" : "https://medium.com/@amrojjeh/1024-or-1000-22b7a01efe29?sk=dee2586d3bf0b2bccaa800e9e3035f7d",
+			"Date" : "May 2020"
 		},
 		{
 			"Name" : "Dimensional Analysis",
@@ -36,12 +43,12 @@ var Projects =
 			"Date" : "November 2019"
 		},
 		{
-			"Name" : "1024 or 1000?",
-			"Description" : "There is a misunderstanding that occurs much too often between developers. Is a kilobit 1024 or 1000 bits?",
-			"ImagePath" : "1024 or 1000.png",
-			"Link" : "https://medium.com/@amrojjeh/1024-or-1000-22b7a01efe29?sk=dee2586d3bf0b2bccaa800e9e3035f7d",
-			"Date" : "May 2020"
-		}
+			"Name" : "What Are Moles?",
+			"Description" : "Describes what a mole is and what it isn't, in a single page. Article was ported from my site to Medium for easier access.",
+			"ImagePath" : "Moles.jpg",
+			"Link" : "https://medium.com/@amrojjeh/what-are-moles-ec867a145ad5",
+			"Date" : "November 2018"
+		},
 	]
 }
 
