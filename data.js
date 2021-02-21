ccf7f5cb209e7ced2fed6cfe96620085cc1f9023
dinglydo <amrojjeh@gmail.com>
2021-02-21 04:15:34 -0600

    Added stuff

--- data.js
+++ data.js
@@ -3,6 +3,13 @@ var Projects =
 	"Software": 
 	[
 		{
+			"Name": "Book Club Bot",
+			"Description": "Manages book club things such as nominations and voting",
+			"ImagePath": "https://raw.githubusercontent.com/amrojjeh/BookClubBot/main/default_cover.jpg",
+			"Link": "https://github.com/amrojjeh/BookClubBot",
+			"Date": "January 2021"
+		},
+		{
 			"Name": "Discord Deception Games",
 			"Description": "A Town of Salem inspired discord bot. Includes a bunch of mini-games. Custom games coming soon, almost ready for release.",
 			"ImagePath": "bot.jpg",
@@ -24,8 +31,15 @@ var Projects =
 			"Date": "May 2018"
 		}
 	],
-	"Education":
+	"Computer Science":
 	[
+		{
+			"Name": "Classes",
+			"Description": "And Why We Need Them",
+			"ImagePath": "https://miro.medium.com/max/2400/1*pfVYXitb6stSpP5cfdF5eA.png",
+			"Link": "https://medium.com/codex/classes-b9027a594ba9?sk=1a579f83a6b67a15f1b47ce0c4ba68f4",
+			"Date": "February 2021"
+		},
 		{
 			"Name": "Recursion",
 			"Description": "A simple explanation of recursion",
@@ -36,9 +50,28 @@ var Projects =
 		{
 			"Name": "1024 or 1000?",
 			"Description": "There is a misunderstanding that occurs much too often between developers. Is a killobit 1024 or 1000 bits?",
-			"ImagePath": "https://miro.medium.com/max/2400/1*7ilUOC7hinanyhbc8JkqYA.png",
+			"ImagePath": "https://miro.medium.com/max/2400/0*dvZ97m-KsG_MmvK-",
 			"Link": "https://amrojjeh.medium.com/1024-or-1000-22b7a01efe29?sk=dee2586d3bf0b2bccaa800e9e3035f7d",
 			"Date": "May 2020"
+		},
+	],
+	"Science":
+	[
+		{
+			"Name": "What Are Moles?",
+			"Description": "What are they!",
+			"ImagePath": "https://miro.medium.com/max/700/1*2Y-yAERBFLUJYpy2Ndnpyg.png",
+			"Link": "https://amrojjeh.medium.com/what-are-moles-ec867a145ad5?sk=8ef37774eaca78928d05cb121752cdea",
+			"Date": "November 2019"
+		},
+	],
+	"Humor":
+	[
+		{
+			"Name": "An Open Letter To Medium",
+			"Description": "A Very Personal Letter To Medium",
+			"ImagePath": "https://miro.medium.com/max/700/1*k93pu0wvTHVnBcGJy0NCqA.png",
+			"Date": "January 2021"
 		}
 	]
 }
